9fd367a7cf0d83987f535f9a55f5137268cca340
Shivam <shivam.si1404@gmail.com>
2026-09-22 00:59:53 +0530

    Executables and headers

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include "NovaSearch.h"
 using namespace std;
+
+
 
 int main(int argc, char const *argv[])
 {
-    cout<<"NovaSearch2.0";
+    cout<<"NovaSearch2.0"<<endl;
+
+    helloNovaSearch();
     return 0;
 }
---
+++ src/NovaSearch.cpp
@@ -0,0 +1,8 @@
+// Core implementation of NovaSearch 
+#include "NovaSearch.h"
+#include <iostream>
+
+void helloNovaSearch()
+{
+    std::cout << "NovaSearch core initialized\n";
+}
